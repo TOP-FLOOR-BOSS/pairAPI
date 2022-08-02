a057e08f34feab8d01a201b3b9e603226c388778
TOP-FLOOR-BOSS <engelliam71@gmail.com>
2022-08-02 09:57:15 +0200

    Update

--- config/dbconn.js
+++ config/dbconn.js
@@ -3,7 +3,7 @@ const { createConnection } = require('mysql');
 const connection = createConnection({
     host: process.env.host,
     user: process.env.dbUser,
-    password: process.env.dbPassword,
+    password: process.env.dbPassword, 
     port: process.env.dbPort,
     database: process.env.dbName,
     multipleStatements: true
