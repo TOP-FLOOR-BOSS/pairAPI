f9950b002e13a487f05e7211dcaf11f454448c42
TOP-FLOOR-BOSS <engelliam71@gmail.com>
2022-08-04 11:50:51 +0200

    pls God

--- index.js
+++ index.js
@@ -1,96 +1,110 @@
-// Importing modules
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 const db = require('./config/dbconn');
 const {compare, hash} = require('bcrypt');
-const path = require('path');
 // Express app
 const app = express();
 // Express router
 const router = express.Router();
-// Configuration 
+// Configuration
 const port = parseInt(process.env.PORT) || 4000;
-app.use(router, cors(), express.json(), express.urlencoded({
-    extended: true
-}));
-
+app.use(router, cors(), express.json(),
+    express.urlencoded({
+    extended: true})
+);
+//
 app.listen(port, ()=> {
     console.log(`Server is running on port ${port}`);
 });
-// Home
-router.get('/', (req, res)=>{
+// home
+router.get('/', (req, res)=> {
     res.status(200).sendFile(path.join(__dirname, 'views', 'index.html'));
 });
-router.get('/register', (req, res)=>{
-    res.status(200).sendFile(path.join(__dirname, 'views', 'register.html'));
+router.get('/register', (req, res)=> {
+    res.status(200).sendFile(path.join(__dirname, 'views', 'products.html'));
 });
-
 // User registration
-app.post('/register', bodyParser.json(), async (req, res)=> {
-    const bd = req.body; 
-    // Encrypting a password
-    // Default genSalt() is 10
-    bd.userpassword = await hash(bd.userpassword, 10);
-    // Query
-    const strQry = 
-    `
-    INSERT INTO users(firstname, lastname, gender, address, email, userpassword)
-    VALUES(?, ?, ?, ?, ?, ?);
-    `;
-    //
-    db.query(strQry, 
-        [bd.firstname, bd.lastname, bd.gender, bd.address, bd.email, bd.userpassword],
-        (err, results)=> {
-            if(err) throw err;
-            res.send(`number of affected row/s: ${results.affectedRows}`);
-        })
+app.post('/register',bodyParser.json(),
+    async (req, res)=> {
+    try{
+        const bd = req.body;
+        if(bd.userRole === ' ' || bd.userRole === null) {
+            bd.userRole = 'user';
+        }
+        // Encrypting a password
+        // Default value of salt is 10.
+        bd.userpassword = await hash(bd.userpassword, 10);
+        // Query
+        const strQry =
+        `
+        INSERT INTO users(firstname, lastname, gender, address, userRole, email, userpassword)
+        VALUES(?, ?, ?, ?, ?, ?, ?);
+        `;
+        db.query(strQry,
+            [bd.firstname, bd.lastname, bd.gender, bd.address, bd.userRole,bd.email, bd.userpassword],
+            (err, results)=> {
+                if(err) throw err;
+                res.send(`number of affected row/s: ${results.affectedRows}`);
+            })
+    }catch(e) {
+        console.log(`From registration: ${e.message}`);
+    }
 });
 // Login
-router.post('/login', bodyParser.json(), (req, res)=> {
-    const strQry = 
-    `
-    SELECT firstname, gender, email, userpassword
-    FROM users;
-    `;
-    db.query(strQry, (err, results)=> {
-        if(err) throw err;
-        res.json({
-            status: 200,
-            results: results
-        })
-    })
-/*
-Have to compare: 
-compare(req.body.userpassword, results.userpassword)
-======
-require('crypto').randomBytes(64).toString('hex')
-*/
-})
-
-// Create new products
-router.post('/products', bodyParser.json(), (req, res)=> {
-    const bd = req.body; 
-    bd.totalamount = bd.quantity * bd.price;
-    // Query
-    const strQry = 
-    `
-    INSERT INTO products(prodName, prodUrl, quantity, price, totalamount, dateCreated)
-    VALUES(?, ?, ?, ?, ?, ?);
-    `;
-    //
-    db.query(strQry, 
-        [bd.prodName, bd.prodUrl, bd.quantity, bd.price, bd.totalamount, bd.dateCreated],
-        (err, results)=> {
+router.post('/login', bodyParser.json(),
+    (req, res)=> {
+    try{
+        // Get email and password
+        const { email, userpassword } = req.body;
+        const strQry =
+        `
+        SELECT firstname, gender, email, userpassword
+        FROM users
+        WHERE email = '${email}';
+        `;
+        db.query(strQry, async (err, results)=> {
             if(err) throw err;
-            res.send(`number of affected row/s: ${results.affectedRows}`);
+            res.json({
+                status: 200,
+                results: (await compare(userpassword,
+                    results[0].userpassword)) ? results :
+                    'You provided a wrong email or password'
+            })
         })
+    }catch(e) {
+        console.log(`From login: ${e.message}`);
+    }
+})
+// Create new products
+router.post('/products', bodyParser.json(),
+    (req, res)=> {
+    try{
+        const bd = req.body;
+        bd.totalamount = bd.quantity * bd.price;
+        // Query
+        const strQry =
+        `
+        INSERT INTO products(prodName, prodUrl, quantity, price, totalamount, dateCreated)
+        VALUES(?, ?, ?, ?, ?, ?);
+        `;
+        //
+        db.query(strQry,
+            [bd.prodName, bd.prodUrl, bd.quantity, bd.price, bd.totalamount, bd.dateCreated],
+            (err, results)=> {
+                if(err) throw err
+                res.send(`number of affected row/s: ${results.affectedRows}`);
+            })
+    }catch(e) {
+        console.log(`Create a new product: ${e.message}`);
+    }
 });
 // Get all products
 router.get('/products', (req, res)=> {
     // Query
-    const strQry = 
+    const strQry =
     `
     SELECT id, prodName,prodUrl, quantity, price, totalamount, dateCreated, userid
     FROM products;
@@ -101,13 +115,13 @@ router.get('/products', (req, res)=> {
             status: 200,
             results: results
         })
+        console.log(err)
     })
 });
-
 // Get one product
 router.get('/products/:id', (req, res)=> {
     // Query
-    const strQry = 
+    const strQry =
     `
     SELECT id, prodName, prodUrl, quantity, price, totalamount, dateCreated, userid
     FROM products
@@ -122,26 +136,24 @@ router.get('/products/:id', (req, res)=> {
     })
 });
 // Update product
-router.put('/products', bodyParser.json(), (req, res)=> {
+router.put('/products', (req, res)=> {
     const bd = req.body;
     // Query
-    const strQry = 
+    const strQry =
     `UPDATE products
      SET ?
      WHERE id = ?`;
-
     db.query(strQry,[bd.id], (err, data)=> {
         if(err) throw err;
         res.send(`number of affected record/s: ${data.affectedRows}`);
     })
 });
-
 // Delete product
 router.delete('/clinic/:id', (req, res)=> {
     // Query
-    const strQry = 
+    const strQry =
     `
-    DELETE FROM products 
+    DELETE FROM products
     WHERE id = ?;
     `;
     db.query(strQry,[req.params.id], (err, data, fields)=> {
@@ -149,9 +161,4 @@ router.delete('/clinic/:id', (req, res)=> {
         res.send(`${data.affectedRows} row was affected`);
     })
 });
-/*
-res.status(200).json({
-    status: 200,
-    results: results
-})
-*/
+console.log(bodyParser.json());
